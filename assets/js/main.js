b56769827b94bca493551dbb2f6a92a268413ec8
kush002 <kushagrasingh543@gmail.com>
2023-04-04 05:28:11 +0530

    Add files via upload

--- assets/js/main.js
+++ assets/js/main.js
@@ -7,6 +7,7 @@ const showMenu = (toggleId, navId) => {
 
   if (true) {
     toggle.addEventListener("click", function () {
+      console.log("clicked");
       nav.classList.toggle("show-menu");
     });
   }
@@ -14,53 +15,53 @@ const showMenu = (toggleId, navId) => {
 
 showMenu("nav-toggle", "nav-menu");
 
-/*================ SWIPER JS ====================*/
+// /*================ SWIPER JS ====================*/
 
-let galleryThumbs = new Swiper(".gallery-thumbs", {
-  spaceBetween: 0,
-  slidesPerView: 0,
-});
+// let galleryThumbs = new Swiper(".gallery-thumbs", {
+//   spaceBetween: 0,
+//   slidesPerView: 0,
+// });
 
-let galleryTop = new Swiper(".gallery-top", {
-  effect: "fade",
-  loop: true,
+// let galleryTop = new Swiper(".gallery-top", {
+//   effect: "fade",
+//   loop: true,
 
-  thumbs: {
-    swiper: galleryThumbs,
-  },
-});
+//   thumbs: {
+//     swiper: galleryThumbs,
+//   },
+// });
 
-/*================ GSAP ANIMATION ====================*/
+// /*================ GSAP ANIMATION ====================*/
 
-const controlImg = document.querySelectorAll(".controls__img");
+// const controlImg = document.querySelectorAll(".controls__img");
 
-function ScrollAnimation() {
-  gsap.from(".cottage__subtitle", {
-    opacity: 0,
-    duration: 0.2,
-    delay: 0.2,
-    y: -20,
-  });
-  gsap.from(".cottage__title", {
-    opacity: 0,
-    duration: 0.3,
-    delay: 0.3,
-    y: -20,
-  });
-  gsap.from(".cottage__description", {
-    opacity: 0,
-    duration: 0.4,
-    delay: 0.4,
-    y: -20,
-  });
-  gsap.from(".cottage__button", {
-    opacity: 0,
-    duration: 0.5,
-    delay: 0.5,
-    y: -20,
-  });
-}
-controlImg.forEach((c) => c.addEventListener("click", ScrollAnimation));
+// function ScrollAnimation() {
+//   gsap.from(".cottage__subtitle", {
+//     opacity: 0,
+//     duration: 0.2,
+//     delay: 0.2,
+//     y: -20,
+//   });
+//   gsap.from(".cottage__title", {
+//     opacity: 0,
+//     duration: 0.3,
+//     delay: 0.3,
+//     y: -20,
+//   });
+//   gsap.from(".cottage__description", {
+//     opacity: 0,
+//     duration: 0.4,
+//     delay: 0.4,
+//     y: -20,
+//   });
+//   gsap.from(".cottage__button", {
+//     opacity: 0,
+//     duration: 0.5,
+//     delay: 0.5,
+//     y: -20,
+//   });
+// }
+// controlImg.forEach((c) => c.addEventListener("click", ScrollAnimation));
 
 /*================ Dynamic Data ====================*/
 const timer = function (inputClass, time) {
@@ -70,7 +71,7 @@ const timer = function (inputClass, time) {
   const updateNum = () => {
     const target = nums.getAttribute("data-target");
     const count = +nums.innerText;
-    const increment = target / 350;
+    const increment = target / 345;
     if (count < target) {
       nums.innerText = `${Math.ceil(count + increment)}`;
       setTimeout(updateNum, time);
@@ -84,20 +85,26 @@ const timer = function (inputClass, time) {
 
 const countingNum = function (entries) {
   const [entry] = entries;
+  console.log(entry);
 
   if (!entry.isIntersecting) return;
-  timer(".numbers-1", 250);
-  timer(".numbers-2", 1);
-  timer(".numbers-3", 45);
-  timer(".numbers-4", 0.1);
+  setTimeout(() => {
+    timer(".numbers-1", 250);
+    timer(".numbers-2", 1);
+    timer(".numbers-3", 45);
+    timer(".numbers-4", 0.1);
+  }, 400);
 
   observer.unobserve(entry.target);
 };
-
+const x = window.matchMedia("(max-width:767px)", "(max-height:700px)");
+let thresholdValue = 0.75;
+if (x.matches) thresholdValue = 0.34;
+console.log(thresholdValue);
 const section_1 = document.querySelector(".nearby");
 const observer = new IntersectionObserver(countingNum, {
   root: null,
-  threshold: 0.85,
+  threshold: thresholdValue,
 });
 
 observer.observe(section_1);
@@ -147,12 +154,12 @@ const displayPlaces = function (places, f_num) {
   places.forEach((place, i) => {
     const html = `
 <div class="individual__place__container_${i + 1}">
-<div class="placeItem__container">
+<div class="placeItem__container_${i + 1}">
   <div class="img_container">
     <img src="assets/img/places-${f_num}/place-img-${i}.jpg" alt="image${i}" class="place__img" />
   </div>
   <div class="place__title">
-    <h2 class="place_titleText">${place.name}</h2>
+    <h4 class="place_titleText">${place.name}</h4>
   </div>
   <div class="place__description">
     <p class="place__descriptionText">${place.description}</p>
